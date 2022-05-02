9ae12d9c9b781f11b98f2249e5bc75b2948556ca
tushar <tushar@nowhere.com>
2022-05-02 12:32:29 +0530

    Initial commit

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ THIS_DIR=$(cd "$(dirname "$0")" && pwd )
 cd "$THIS_DIR" || exit
 
 # Normal run
-docker run -it --rm --name unbound --dns 1.1.1.1 -p 53:5335/udp -p 53:5335/tcp -v "$PWD"/unbound.conf:/etc/unbound/unbound.conf.d/unbound.conf unbound
+docker run -it --rm --name unbound --dns 1.1.1.1 -p 5335:5335/udp -p 5335:5335/tcp -v "$PWD"/unbound.conf:/etc/unbound/unbound.conf.d/unbound.conf unbound
 
 # Run on privileged port 53 with root
 #sudo docker run -it --rm --name unbound --dns 1.1.1.1 -p 53:5335/udp -p 53:5335/tcp -v "$PWD"/unbound.conf:/etc/unbound/unbound.conf.d/unbound.conf unbound
